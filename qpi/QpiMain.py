1038c01b4fcc937bf18e3c8c4c4830602f6225ac
Sujith S <sujithx007@gmail.com>
2022-02-13 06:39:08 +0530

    Update QpiMain.py

--- qpi/QpiMain.py
+++ qpi/QpiMain.py
@@ -8,7 +8,11 @@ Version : V 0.0.1
 The main classes and functions for QPI Rest
 '''
 class Qpi:
-    
+    '''
+    The main class for QPI Rest contains the functions to
+    Take model and parameters as input and output the 
+    REST API endpoint
+    '''    
     def __init__(self, a, b) -> None:
         self.a = a
         self.b = b
